c9110f3e9db866333c8d80ec2245b68b7042c679
Arno Teigseth <arnotixe@gmail.com>
2017-04-26 09:58:31 +0200

    basic qr code read

--- index.php
+++ index.php
@@ -2,5 +2,23 @@
 
 // STOMA
 
+// login in cookie
+session_start(); // start/resume session
+
+//setcookie("teigseth.no/fv", "user", time()+60*60*24*30);
+
+
+// Redirect to login if not
+
+
+
+
+// when logged in,
+
+// qr code parameter: Assign tool to yerself
+$t=000001 // max no. of tools is one million per server
+
+echo "Assign Tool no $_GET[t] to logged-in user";
+
 
 ?>
